437ca0561dd27a75e4d3e8bfceee76d6cea85e3b
mshumailk <160420494+mshumailk@users.noreply.github.com>
2024-03-29 11:13:47 +0500

    number_guessing_game

---
+++ number_guessing_game.js
@@ -0,0 +1,19 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+// System will generate randon number
+//any input from user (number)
+//match randon and user numbers then show result
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "Please Guess any number between 1-6: ",
+    },
+]);
+if (answer.userguessednumber === randomnumber) {
+    console.log("You guessed rite number: ");
+}
+else {
+    console.log("Sorry try again");
+}
